e87245cafd70c0d282caef283573e1edd75d62dd
Ian Spence <ian@ecnepsnai.com>
2016-10-01 11:09:26 -0700

    Add increment script

---
+++ Increment.rb
@@ -0,0 +1,24 @@
+begin
+    Gem::Specification.find_by_name('plist')
+rescue
+    puts 'The plist gem is required to use this sync utility.'
+    puts 'Install using: `[sudo] gem install plist`'
+    exit 0
+end
+
+require 'plist'
+
+$currentVersion = 0
+
+$pwd = File.expand_path(File.dirname(__FILE__))
+
+["Certificate Inspector/Info.plist", "Inspect Website/Info.plist"].each do |path|
+    properties = Plist::parse_xml("#{$pwd}/#{path}")
+    $currentVersion = properties["CFBundleVersion"].to_i
+    $currentVersion = $currentVersion += 1
+    properties["CFBundleVersion"] = $currentVersion.to_s
+
+    File.write("#{$pwd}/#{path}", properties.to_plist)
+    `git add "#{path}"`
+end
+`git commit -m "Increment build to #{$currentVersion}"`
