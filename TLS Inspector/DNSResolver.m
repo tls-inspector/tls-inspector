f5f42ddeb24bf8282780a465bf3a6d2e5b3b1ab6
Ian Spence <ian@ecnepsnai.com>
2017-03-17 22:38:02 -0700

    Fix DNS resolver

--- TLS Inspector/DNSResolver.m
+++ TLS Inspector/DNSResolver.m
@@ -8,28 +8,32 @@
 @implementation DNSResolver
 
 + (NSArray<NSString *> *) resolveHostname:(NSString *)hostname error:(NSError **)error {
-    struct addrinfo hints, *servinfo, *p;
+    struct addrinfo* result;
+    struct addrinfo* res;
     int rv;
+
+    const char * name = [hostname UTF8String];
     
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    
-    if ((rv = getaddrinfo([hostname UTF8String], "https", &hints, &servinfo)) != 0) {
+    rv = getaddrinfo(name, NULL, NULL, &result);
+    if (rv != 0) {
         const char * errorString = gai_strerror(rv);
         * error = [NSError errorWithDomain:@"DNSResolver" code:rv userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithUTF8String:errorString]}];
         return nil;
     }
     
     NSMutableArray<NSString *> * addresses = [NSMutableArray new];
-    
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        struct sockaddr_in * ipv4 = (struct sockaddr_in *)p->ai_addr;
-        char ipAddress[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(ipv4->sin_addr), ipAddress, INET_ADDRSTRLEN);
-        [addresses addObject:[NSString stringWithUTF8String:ipAddress]];
-    }
 
+    for (res = result; res != NULL; res = res->ai_next) {
+        // Shodan doesn't have much results for IPv6 hosts, so just sticking with A records for now.
+        if (res->ai_family == AF_INET) {
+            struct sockaddr_in * ipv4 = (struct sockaddr_in *)res->ai_addr;
+            char ipAddress[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &(ipv4->sin_addr), ipAddress, INET_ADDRSTRLEN);
+            [addresses addObject:[NSString stringWithUTF8String:ipAddress]];
+        }
+    }
+    
+    freeaddrinfo(result);
     return addresses;
 }
 
