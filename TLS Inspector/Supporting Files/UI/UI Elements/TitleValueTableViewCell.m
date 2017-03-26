93747e9093ea75030b0c4f91897e97824d5f8439
Ian Spence <ian@ecnepsnai.com>
2017-03-26 11:31:54 -0700

    Fixed line count for TitleValue table cells

--- TLS Inspector/Supporting Files/UI/UI Elements/TitleValueTableViewCell.m
+++ TLS Inspector/Supporting Files/UI/UI Elements/TitleValueTableViewCell.m
@@ -4,7 +4,7 @@
 
 - (CGFloat) heightForCell {
     float base = 70.0f;
-    int numberOfLines = [self lineCountForLabel:self.valueLabel];
+    NSUInteger numberOfLines = [self lineCountForLabel:self.valueLabel];
     if (numberOfLines > 1) {
         return base + (10.0f * numberOfLines);
     } else {
@@ -12,14 +12,37 @@
     }
 }
 
-- (int) lineCountForLabel:(UILabel *)label {
-    NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:label.text attributes:@{NSFontAttributeName: label.font}];
-    CGRect rect = [attributedText boundingRectWithSize:(CGSize){label.frame.size.width, CGFLOAT_MAX}
-                                               options:NSStringDrawingUsesLineFragmentOrigin
-                                               context:nil];
-    CGSize size = rect.size;
+- (NSUInteger) lineCountForLabel:(UILabel *)label {
+    NSTextStorage * textStorage = [[NSTextStorage alloc] initWithString:label.text attributes:@{NSFontAttributeName: label.font}];
     
-    return (int)ceil(size.height / label.font.lineHeight);
+    // The labels width appears to always be the full width of the superview (cell) when this method is called
+    // so we manually subtrack the padding of the label from the width
+    float padding;
+    if (isCompact) {
+        padding = 16.0f;
+    } else {
+        padding = 12.0f;
+    }
+    CGFloat width = label.frame.size.width - padding;
+    CGSize size = (CGSize){width, label.frame.size.height};
+    NSTextContainer * textContainer = [[NSTextContainer alloc] initWithSize:size];
+    textContainer.lineBreakMode = NSLineBreakByWordWrapping;
+    textContainer.maximumNumberOfLines = 0;
+    textContainer.lineFragmentPadding = 0;
+    
+    NSLayoutManager * layoutManager = [NSLayoutManager new];
+    layoutManager.textStorage = textStorage;
+    [layoutManager addTextContainer:textContainer];
+    
+    NSUInteger numberOfLines = 0, index = 0;
+    NSRange lineRange = NSMakeRange(0, 0);
+    
+    for (; index < layoutManager.numberOfGlyphs; numberOfLines++) {
+        [layoutManager lineFragmentRectForGlyphAtIndex:index effectiveRange:&lineRange];
+        index = NSMaxRange(lineRange);
+    }
+    
+    return numberOfLines;
 }
 
 @end
