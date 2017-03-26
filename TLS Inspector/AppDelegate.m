1e5125ce31a5ff82878617bb56714774bcd26cdd
Ian Spence <ian@ecnepsnai.com>
2017-03-26 10:12:38 -0700

    TableViewCells have white background on iPads running iOS 9 #44

--- TLS Inspector/AppDelegate.m
+++ TLS Inspector/AppDelegate.m
@@ -9,7 +9,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // Style the navigation bar
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.141f green:0.204f blue:0.278f alpha:1.0f]];
+    [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:0.114f green:0.631f blue:0.949f alpha:1.0f]];
+    [[UINavigationBar appearance] setBackgroundColor:[UIColor colorWithRed:0.141f green:0.204f blue:0.278f alpha:1.0f]];
 
+    // Style the tableview
+    [[UITableView appearance] setBackgroundColor:[UIColor colorWithRed:0.196f green:0.27f blue:0.329f alpha:1.0f]];
+    [[UITableView appearance] setSeparatorColor:[UIColor colorWithRed:0.078f green:0.114f blue:0.149f alpha:1.0f]];
+    
+    // Style the tableview cells
+    [[UITableViewCell appearance] setBackgroundColor:[UIColor colorWithRed:0.106f green:0.157f blue:0.212f alpha:1.0f]];
     UIView *selectionView = [UIView new];
     selectionView.backgroundColor = [UIColor colorWithRed:0.08 green:0.12 blue:0.16 alpha:1.0];
     [[UITableViewCell appearance] setSelectedBackgroundView:selectionView];
