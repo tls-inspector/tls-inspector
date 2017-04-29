00adfc65f3b718647a88d9ceec09b03c4394b48b
Ian Spence <ian@ecnepsnai.com>
2017-04-29 11:05:48 -0700

    Merge scripts

--- TLS Inspector/Localization/Strings/sort.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-for FILE in *.plist; do
-    plutil -convert binary1 en.plist && plutil -convert xml1 $FILE
-    sed -i '' "s/'/\&apos;/g" $FILE
-done
---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+mode=$1;
+case "$mode" in
+    increment)
+        pushd `dirname $0` > /dev/null
+        SCRIPTPATH=`pwd -P`
+        popd > /dev/null
+
+
+        info_plists=("${SCRIPTPATH}/TLS Inspector/Info.plist" "${SCRIPTPATH}/Inspect Website/Info.plist")
+        for ((i = 0; i < ${#info_plists[@]}; i++)); do
+            info_plist=${info_plists[i]}
+
+            current_build_number=$(defaults read "${info_plist}" CFBundleVersion)
+            new_build_number=$(($current_build_number + 1))
+            echo "Build number: '$current_build_number' -> '$new_build_number'."
+            defaults write "${info_plist}" CFBundleVersion $new_build_number
+            plutil -convert xml1 "${info_plist}"
+        done
+        ;;
+    lang)
+        if [ -z "$2" ]; then
+            echo "Usage: $0 lang <strings plist file>"
+            exit 1
+        fi
+
+        plutil -convert binary1 "$2" && plutil -convert xml1 "$2" && sed -i '' "s/'/\&apos;/g" "$2"
+        ;;
+esac
