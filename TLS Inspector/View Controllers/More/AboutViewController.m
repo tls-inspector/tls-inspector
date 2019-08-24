cffea6be0f800ee2af7d7f37235d517e6184aa83
Ian Spence <ian@ecn.io>
2019-08-23 20:27:11 -0700

    Use AppLinks when submitting logs

--- TLS Inspector/View Controllers/More/AboutViewController.m
+++ TLS Inspector/View Controllers/More/AboutViewController.m
@@ -119,7 +119,7 @@ static NSString * PROJECT_TESTFLIGHT_APPLICATION = @"https://tlsinspector.com/be
                      break;
                  case 1: {
                      [ContactSupportTableViewController collectFeedbackOnController:self finished:^(NSString *comments) {
-                         [self.appLinks showEmailComposeSheetForAppInViewController:self withComments:comments dismissed:nil];
+                         [self.appLinks showEmailComposeSheetForAppInViewController:self withComments:comments includeLogs:NO dismissed:nil];
                      }];
                      break;
                  } default:
