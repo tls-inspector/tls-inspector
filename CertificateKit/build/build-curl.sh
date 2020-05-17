4b67a81209c23e0b967b883723b1e8986d7e05de
Ian Spence <ian@ecnepsnai.com>
2020-05-17 14:36:40 -0700

    Update build scripts

--- CertificateKit/build/build-curl.sh
+++ CertificateKit/build/build-curl.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 set -e
 
+# Because we run this script from a Xcode build phase, it adds a million and one of it's own
+# environment variables, which mess with compiling the library. To get around this, we run
+# this script without any variables (env -i <script>), but need to provide a default PATH
+# so that coreutils will still work
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+
 CURL_WANT_VERSION=$(grep 'VERSION' curl.want | cut -d '=' -f 2)
-CURL_WANT_HASH=$(grep 'HASH' curl.want | cut -d '=' -f 2)
 
 echo "cURL version wanted: ${CURL_WANT_VERSION}"
-echo "cURL hash wanted: ${CURL_WANT_HASH}"
 
-if [ -f ../curl.framework/curl ]; then
-    CURRENT_HASH=$(shasum -a 512 ../curl.framework/curl | cut -d ' ' -f 1)
-    if [ "${CURRENT_HASH}" == "${CURL_WANT_HASH}" ]; then
+if [ -f ../curl.framework/Headers/curlver.h ]; then
+    CURRENT_VERSION=$(grep 'LIBCURL_VERSION' ../curl.framework/Headers/curlver.h | egrep -o '"[a-zA-Z0-9\.\-]+"' | tr -d '"')
+    if [ "${CURRENT_VERSION}" == "${CURL_WANT_VERSION}" ]; then
         echo "cURL already built, nothing to do"
         exit 0
     else
-        echo "warning: cURL hash did not match. recompiling version ${CURL_WANT_VERSION}"
+        echo "warning: cURL version did not match. recompiling version ${CURL_WANT_VERSION}"
         rm -r ../curl.framework
     fi
+else
+    echo "warning: cURL needs to be compiled. This will take a while..."
 fi
 
 cd libcurl-ios
-./build-curl.sh ${CURL_WANT_VERSION}
+rm -rf build/ output/
+./build-ios.sh ${CURL_WANT_VERSION}
+mv curl.framework ../../
 cd ../
-mv curl.framework ../
 
-if [ -f ../curl.framework/curl ]; then
-    if [ "${CURRENT_HASH}" == "${CURL_WANT_HASH}" ]; then
+if [ -f ../curl.framework/Headers/curlver.h ]; then
+    CURRENT_VERSION=$(grep 'LIBCURL_VERSION' ../curl.framework/Headers/curlver.h | egrep -o '"[a-zA-Z0-9\.\-]+"' | tr -d '"')
+    if [ "${CURRENT_VERSION}" == "${CURL_WANT_VERSION}" ]; then
         exit 0
     fi
 fi
 
-echo "error: cURL hash did not match after building version ${CURL_WANT_VERSION}"
+echo "error: cURL version did not match after building version ${CURL_WANT_VERSION}"
 exit 1
--- CertificateKit/build/build-openssl.sh
+++ CertificateKit/build/build-openssl.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 set -e
 
+# Because we run this script from a Xcode build phase, it adds a million and one of it's own
+# environment variables, which mess with compiling the library. To get around this, we run
+# this script without any variables (env -i <script>), but need to provide a default PATH
+# so that coreutils will still work
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+
 OPENSSL_WANT_VERSION=$(grep 'VERSION' openssl.want | cut -d '=' -f 2)
-OPENSSL_WANT_HASH=$(grep 'HASH' openssl.want | cut -d '=' -f 2)
 
 echo "OpenSSL version wanted: ${OPENSSL_WANT_VERSION}"
-echo "OpenSSL hash wanted: ${OPENSSL_WANT_HASH}"
 
-if [ -f ../openssl.framework/openssl ]; then
-    CURRENT_HASH=$(shasum -a 512 ../openssl.framework/openssl | cut -d ' ' -f 1)
-    if [ "${CURRENT_HASH}" == "${OPENSSL_WANT_HASH}" ]; then
+if [ -f ../openssl.framework/Headers/opensslv.h ]; then
+    CURRENT_VERSION=$(grep 'OPENSSL_FULL_VERSION_STR' ../openssl.framework/Headers/opensslv.h | egrep -o '"[a-zA-Z0-9\.\-]+"' | tr -d '"')
+    if [ "${CURRENT_VERSION}" == "${OPENSSL_WANT_VERSION}" ]; then
         echo "OpenSSL already built, nothing to do"
         exit 0
     else
-        echo "warning: OpenSSL hash did not match. recompiling version ${OPENSSL_WANT_VERSION}"
+        echo "warning: OpenSSL version did not match. recompiling version ${OPENSSL_WANT_VERSION}"
         rm -r ../openssl.framework
     fi
+else
+    echo "warning: OpenSSL needs to be compiled. This will take a while..."
 fi
 
 cd openssl-ios
+rm -rf build/ output/
 ./build-openssl.sh ${OPENSSL_WANT_VERSION}
+mv openssl.framework ../../
 cd ../
-mv openssl.framework ../
 
-if [ -f ../openssl.framework/openssl ]; then
-    if [ "${CURRENT_HASH}" == "${OPENSSL_WANT_HASH}" ]; then
+if [ -f ../openssl.framework/Headers/opensslv.h ]; then
+    CURRENT_VERSION=$(grep 'OPENSSL_FULL_VERSION_STR' ../openssl.framework/Headers/opensslv.h | egrep -o '"[a-zA-Z0-9\.\-]+"' | tr -d '"')
+    if [ "${CURRENT_VERSION}" == "${OPENSSL_WANT_VERSION}" ]; then
         exit 0
     fi
 fi
 
-echo "error: OpenSSL hash did not match after building version ${OPENSSL_WANT_VERSION}"
+echo "error: OpenSSL version did not match after building version ${OPENSSL_WANT_VERSION}"
 exit 1
