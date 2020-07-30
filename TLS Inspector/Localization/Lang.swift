6e6d08572927be5d126ca5f6bfc1ec5aba0c4ecc
Ian Spence <ian@ecnepsnai.com>
2020-07-29 19:08:06 -0700

    Imported language loading

--- TLS Inspector/Localization/Lang.swift
+++ TLS Inspector/Localization/Lang.swift
@@ -4,25 +4,55 @@ private var LOCALIZATION_DICTIONARY: [String: String]?
 
 class Localization {
     public static func load() {
-        let preferredLang = Locale.preferredLanguages[0]
-        var lang: String = "en"
-        for supportedLanguage in ["en"] {
-            if preferredLang.hasPrefix(supportedLanguage) {
-                lang = supportedLanguage
-            }
+        // Always load english, overwrite the keys with translated values if they exist
+        LOCALIZATION_DICTIONARY = loadStringDictionary(name: "en")
+
+        if Locale.preferredLanguages.count <= 0 {
+            // This really shouldn't happen but it could crash the app otherwise
+            print("Preferred language is empty")
+            return
         }
 
-        guard let path = Bundle.main.path(forResource: lang, ofType: "plist") else {
-            print("No language plist found for language '\(lang)'")
+        // We're not terribly concerned with the locale-specifics of languages (at the moment)
+        // so just focus on the language and trim off the locale (I.E. we don't have specific english files for
+        // Canadian english v.s. British English, we just have english.
+        //
+        // Also Colour is spelt with a U. Deal with it.
+        let preferredLang = Locale.preferredLanguages[0].components(separatedBy: "-")[0]
+        if preferredLang == "en" {
+            // Nothing else to do
             return
+        }
+
+        // Map of supported language - these need to match the filename of the plist
+        let supportedLanguages = [
+            "en": 1,
+            "de": 1
+        ]
+        if supportedLanguages[preferredLang] == nil {
+            print("Unsupported system language '\(preferredLang)'")
+            return
+        }
+
+        if let dict = loadStringDictionary(name: preferredLang) {
+            // Merge the dictionary with the language
+            // so that missing keys always default to the english keys
+            LOCALIZATION_DICTIONARY?.merge(dict, uniquingKeysWith: { (_, new) -> String in new })
+        }
+    }
+
+    private static func loadStringDictionary(name: String) -> [String: String]? {
+        guard let path = Bundle.main.path(forResource: name, ofType: "plist") else {
+            print("No language plist found for language '\(name)'")
+            return nil
         }
 
         guard let dict = NSDictionary(contentsOfFile: path) as? [String : String] else {
-            print("Unable to load language dictionary at path '\(path)'")
-            return
+            print("Unable to load language dictionary at path '\(name)'")
+            return nil
         }
 
-        LOCALIZATION_DICTIONARY = dict
+        return dict
     }
 }
 
