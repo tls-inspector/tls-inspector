913646802de83b153c172e4704c328e32aa98513
Ian Spence <ian@ecnepsnai.com>
2020-10-16 21:04:30 -0700

    Update project URL

--- CertificateKit/CKCertificateChain.h
+++ CertificateKit/CKCertificateChain.h
@@ -4,7 +4,7 @@
 //  MIT License
 //
 //  Copyright (c) 2017 Ian Spence
-//  https://github.com/certificate-helper/CertificateKit
+//  https://tlsinspector.com/github.html
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
