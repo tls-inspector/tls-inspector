65c6c8ee91fdca41f5829e23d60beda00b4afbfd
Ian Spence <ian@ecnepsnai.com>
2022-09-30 21:28:27 -0700

    Update OpenSSL to 3.0.5

--- TLS Inspector/AppDelegate.swift
+++ TLS Inspector/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CertificateKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UserOptions.loadDefaults()
+        CertificateKit.openSSLInit()
         return true
     }
 
