7366c89670837b46394c3a680d10ef198f99fe93
Ian Spence <ian@ecnepsnai.com>
2023-08-22 20:06:08 -0700

    Remove broken UI Tests

--- TLS InspectorUITests/TestUtils.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-func saveScreenshot(_ name: String) -> XCTAttachment {
-    let fullScreenshot = XCUIScreen.main.screenshot()
-    let screenshot = XCTAttachment(screenshot: fullScreenshot)
-    screenshot.lifetime = .keepAlways
-    screenshot.name = name
-    return screenshot
-}
