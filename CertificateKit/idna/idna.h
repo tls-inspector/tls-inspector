78b7138e54fb98afaca98c47fb88a6b382b58c00
Ian Spence <ian@ecnepsnai.com>
2023-12-01 15:10:06 -0800

    Add idna-cocoa submodule

---
+++ CertificateKit/idna/idna.h
@@ -0,0 +1,19 @@
+//
+//  idna.h
+//  idna
+//
+//  Created by Ian Spence on 2023-12-01.
+//  Copyright © 2023 Ian Spence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for idna.
+FOUNDATION_EXPORT double idnaVersionNumber;
+
+//! Project version string for idna.
+FOUNDATION_EXPORT const unsigned char idnaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <idna/PublicHeader.h>
+
+
