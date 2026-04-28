0d490ac15e73d5c13429491eac24f10f2a66e259
Ian Spence <ian@ecnepsnai.com>
2026-04-28 16:02:26 -0700

    Update CT logs and anchors

--- tlskit/Sources/TLSKit/External/Anchors/AnchorVersion.swift
+++ tlskit/Sources/TLSKit/External/Anchors/AnchorVersion.swift
@@ -19,9 +19,9 @@ import Foundation
 // This file is modified by the update-root-ca-bundles.sh script
 
 #if DEBUG
-nonisolated(unsafe) internal var EmbeddedAnchorBundleVersion = "bundle_20260319"
+nonisolated(unsafe) internal var EmbeddedAnchorBundleVersion = "bundle_20260422"
 #else
-internal let EmbeddedAnchorBundleVersion = "bundle_20260319"
+internal let EmbeddedAnchorBundleVersion = "bundle_20260422"
 #endif
 
 internal let AnchorBundleSigningKey = """
