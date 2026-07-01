8d37bf39e44207521a0c300dac7623e5436872c8
Ian Spence <ian@ecnepsnai.com>
2026-07-01 11:34:35 -0700

    Update resources

--- tlskit/Sources/TLSKit/External/Anchors/AnchorVersion.swift
+++ tlskit/Sources/TLSKit/External/Anchors/AnchorVersion.swift
@@ -19,9 +19,9 @@ import Foundation
 // This file is modified by the update-root-ca-bundles.sh script
 
 #if DEBUG
-nonisolated(unsafe) internal var EmbeddedAnchorBundleVersion = "bundle_20260429"
+nonisolated(unsafe) internal var EmbeddedAnchorBundleVersion = "bundle_20260629"
 #else
-internal let EmbeddedAnchorBundleVersion = "bundle_20260429"
+internal let EmbeddedAnchorBundleVersion = "bundle_20260629"
 #endif
 
 internal let AnchorBundleSigningKey = """
